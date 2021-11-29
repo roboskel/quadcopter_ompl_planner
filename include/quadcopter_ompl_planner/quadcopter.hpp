29bbf57ee3e89e8d19a402b312b9e74e39d923c0
George Stavrinos <gstavrinos@iit.demokritos.gr>
2021-11-29 14:59:57 +0200

    Blindly added all algebra functions.
    Unsafe and untested. More coming soon.

--- include/quadcopter_ompl_planner/quadcopter.hpp
+++ include/quadcopter_ompl_planner/quadcopter.hpp
@@ -12,27 +12,35 @@ class Quadcopter {
         void step(/*TODO*/);
 
     protected:
-        void thrust();
-        void torque();
-        void rotation();
-        void acceleration();
-        void thetadot2omega();
-        void omega2thetadot();
-        void angular_acceleration();
+        void thetadot2omega(const Eigen::Vector3d&);
+        void omega2thetadot(const Eigen::Vector3d&);
+        void angular_acceleration(const Eigen::Vector4d&);
+        void rotation(const Eigen::Vector3d&);
+        void thrust(const Eigen::Vector4d&);
+        void torques(const Eigen::Vector4d&);
+        void acceleration(const Eigen::Vector4d&, const Eigen::Vector3d&);
 
     private:
         // Quadcopter properties
         // arm length, mass, thrust coefficient
         double L, m, k;
-        // NOTE physics (might need to remove them or aplpy them to ODE)
-        // gravity, drag coefficient
-        double g, kd;
+        // NOTE physics (might need to remove them or apply them to ODE)
+        // gravity, drag coefficient, global drag coefficient
+        double g, b, kd;
+        Eigen::Vector3d a;
         Eigen::Vector3d x;
+        Eigen::RowVector3d T;
+        Eigen::RowVector3d Fd;
+        Eigen::Vector3d tau;
         Eigen::Vector3d xdot;
+        Eigen::Vector3d xdot_;
         Eigen::Vector3d theta;
         Eigen::Vector3d thetadot;
-        Eigen::Matrix3d omega;
+        Eigen::Vector3d omega;
+        Eigen::Vector3d omegadot;
         Eigen::Matrix3d R;
+        Eigen::DiagonalMatrix<double,3,3> I;
+        // Eigen::Matrix3d I;
 
 };
 }
--- src/quadcopter.cpp
+++ src/quadcopter.cpp
@@ -1,2 +1,61 @@
 #include <quadcopter_ompl_planner/quadcopter.hpp>
 
+namespace quadcopter_ompl {
+
+    void Quadcopter::rotation(const Eigen::Vector3d& angles) {
+        const auto psi = angles(0);
+        const auto theta = angles(1);
+        const auto phi = angles(2);
+        R << 
+            cos(phi) * cos(theta), cos(phi) * sin(theta) * sin(psi) - cos(psi) * sin(phi), sin(phi) * sin(psi) + cos(phi) * cos(psi) * sin(theta),
+            cos(theta) * sin(phi), cos(phi) * cos(psi) + sin(phi) * sin(theta) * sin(psi), cos(psi) * sin(phi) * sin(theta) - cos(phi) * sin(psi),
+            - sin(theta), cos(theta) * sin(psi), cos(theta) * cos(psi);
+    }
+
+    void Quadcopter::thrust(const Eigen::Vector4d& inputs) {
+        T << 0.0, 0.0, k * inputs.sum();
+    }
+
+    void Quadcopter::torques(const Eigen::Vector4d& inputs) {
+        tau << L * k * (inputs(1) - inputs(3)),
+               L * k * (inputs(2) - inputs(4)),
+               b * (inputs(1) - inputs(2) + inputs(3) - inputs(4));
+    }
+
+    void Quadcopter::acceleration(const Eigen::Vector4d& inputs, const Eigen::Vector3d& angles) {
+        rotation(angles);
+        thrust(inputs);
+        T *= R;
+        Fd = -kd * xdot_;
+        a = Eigen::RowVector3d(0,0,-g) + 1/m * T + Fd;
+    }
+
+    void Quadcopter::angular_acceleration(const Eigen::Vector4d& inputs) {
+        torques(inputs);
+        omegadot = I.inverse() * (tau - omega.cross(I * omega));
+    }
+
+    void Quadcopter::thetadot2omega(const Eigen::Vector3d& angles) {
+        const auto phi = angles(0);
+        const auto theta = angles(1);
+        const auto psi = angles(2);
+        Eigen::Matrix3d W;
+        W << 1, 0, -sin(theta),
+             0, cos(phi), cos(theta) * sin(phi),
+             0, -sin(phi), cos(theta) * cos(phi);
+        omega = W * thetadot;
+    }
+
+    void Quadcopter::omega2thetadot(const Eigen::Vector3d& angles) {
+        const auto phi = angles(0);
+        const auto theta = angles(1);
+        const auto psi = angles(2);
+        Eigen::Matrix3d W;
+        W << 1, 0, -sin(theta),
+             0, cos(phi), cos(theta) * sin(phi),
+             0, -sin(phi), cos(theta) * cos(phi);
+        thetadot = W.inverse() * omega;
+    }
+
+}
+
